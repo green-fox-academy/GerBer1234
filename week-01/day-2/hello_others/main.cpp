62abececc4c807ab736890eaf84010b8313d6033
Gergely Berenyi <gergelyberenyi@gmail.com>
2018-11-02 22:03:48 +0100

    Printing to the terminal window

---
+++ week-01/day-2/hello_others/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    std::cout << "Hello, Anti!" << std::endl;
+    std::cout << "Hello, Zoli!" << std::endl;
+    std::cout << "Hello, Andi!" << std::endl;
+
+    return 0;
+}
