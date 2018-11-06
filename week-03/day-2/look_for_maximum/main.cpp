a64033537fca65f69c8f6167c3b6d7530352bc3e
Gergely Berenyi <gergelyberenyi@gmail.com>
2018-11-06 15:25:33 +0100

    Pointers4

--- week-03/day-2/look_for_maximum/main.cpp
+++ week-03/day-2/look_for_maximum/main.cpp
@@ -4,14 +4,21 @@ int main() {
     int number = 0;
     std::cout << "Give me a number, please" << std::endl;
     std::cin >> number;
-    int array[]={};
+    int array[number];
     std::cout << "Give me " << number << " numbers, please" << std::endl;
     for (int i = 0; i < number; i++) {
         int x = 0;
         std::cin >> x;
-        array[i]=x;
+        array[i] = x;
     }
-    std::cout << array[2];
+    int max=0;
+    for (int i = 0; i < number; i++) {
+
+        if(array[i]>max)
+        max=array[i];}
+    std::cout << "The biggest number in the array is " << max << std::endl;
+    std::cout << "The memory adress of it is " << &max << std::endl;
+
     // It should print out the biggest number in the given array and the memory address of it
 
     return 0;
--- week-03/day-2/look_for_value/main.cpp
+++ week-03/day-2/look_for_value/main.cpp
@@ -10,10 +10,6 @@ int main() {
     int y = sizeof(numbers) / sizeof(numbers[0]);
     std::cout << sug(numbers, y, &x) << std::endl;
 
-    // Create a function which takes an array (and it's lenght) and a number as a parameter
-    // the function should return index where it found the given value
-    // if it can't find the number return 0
-
     return 0;
 }
 
