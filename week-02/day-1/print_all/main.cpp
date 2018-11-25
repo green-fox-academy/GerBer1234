d9ead669b00df93d907bfc5ec81b79b9440bd3f0
Gergely Berenyi <gergelyberenyi@gmail.com>
2018-11-25 17:10:09 +0100

    print_all

---
+++ week-02/day-1/print_all/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int numbers[] = {4,5,6,7};
+    for(int i=0;i<sizeof(numbers)/sizeof(numbers[0]);i++){
+        std::cout << numbers[i] << std::endl;
+    }
+    return 0;
+}
