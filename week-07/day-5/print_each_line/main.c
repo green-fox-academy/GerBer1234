aeb10efb6030640a6d2c3e0436be9956cafd6028
Gergely Berenyi <gergelyberenyi@gmail.com>
2018-12-15 20:43:28 +0100

    Print-each-line exercise done

---
+++ week-07/day-5/print_each_line/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char line[100];
+    FILE *fp;
+    fp = fopen("my-file.txt", "r");
+    if (fp == NULL) {
+        printf("Can't open the file!\n");
+        return -1;
+    } else {
+        while (!feof(fp)) {
+            fgets(line, 100, fp);
+            puts(line);
+        }
+    }
+    return 0;
+}
