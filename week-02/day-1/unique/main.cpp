4e74a52665b371f7c87a6b59e8062d8e29a89149
Gergely Berenyi <gergelyberenyi@gmail.com>
2019-01-04 20:20:30 +0100

    Unique exercise refactored

--- week-02/day-1/unique/main.cpp
+++ week-02/day-1/unique/main.cpp
@@ -3,22 +3,19 @@
 
 void printDistinct(int arr[], int n);
 
-int main()
-{
+int main() {
     int arr[] = {1, 11, 34, 11, 52, 61, 1, 34};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
     printDistinct(arr, n);
     return 0;
 }
 
-void printDistinct(int arr[], int n)
-{
+void printDistinct(int arr[], int n) {
     // Pick all elements one by one
-    for (int i=0; i<n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         // Check if the picked element is already printed
         int j;
-        for (j=0; j<i; j++)
+        for (j = 0; j < i; j++)
             if (arr[i] == arr[j])
                 break;
 
