9451b828606a56e21c5e00a397a0aceac55c3fa8
Gergely Berenyi <gergelyberenyi@gmail.com>
2019-01-05 20:19:01 +0100

    Shuffle exercise done

---
+++ week-08/day-4/vector/shuffle/vector.h
@@ -0,0 +1,22 @@
+#ifndef CREATE_VECTOR_VECTOR_H
+#define CREATE_VECTOR_VECTOR_H
+
+typedef struct vector {
+    int size;
+    int capacity;
+    int *elements;
+} vector_t;
+
+vector_t *init_vector();
+
+void add(vector_t *v);
+
+void shuffle(vector_t *v);
+
+/*void push_back(vector_t *v, int value);
+
+void pop_back(vector_t *v);*/
+
+void print_vector(vector_t *v);
+
+#endif //CREATE_VECTOR_VECTOR_H
